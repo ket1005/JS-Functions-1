82c2995f7417b30b0bab42da830af1d1ba93d6c6
Ecaterina Scutelnic <ket1005210052@gmail.com>
2021-07-01 23:43:02 +0300

    final version

---
+++ app.js
@@ -0,0 +1,75 @@
+ //1 
+let Kelvin =293;
+let Fahrenheit;
+
+const kelvinToFahrenheit = Kelvin => Math.floor((Kelvin-273)*(9/5)+32);
+
+console.log(Fahrenheit);
+
+Fahrenheit = kelvinToFahrenheit(Kelvin);
+console.log(Fahrenheit);
+
+//2
+let myAge = 25; //my age
+let earlyYears = 2; // early years
+humanAgeInDogYears = myAge => Math.floor((myAge -2)*4 + (earlyYears*=10.5));
+let myName1 ='Kate';
+let myName = myName1.toLowerCase();
+myAgeInDogYears = humanAgeInDogYears();
+console.log(`My name is ${myName} .I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+//3
+let userName = prompt('Please enter your name');
+userName==''?console.log('Hello,'+ userName):console.log('Hello!');
+let userQuestion = prompt('Please enter your question');
+console.log(userQuestion);
+
+answerMyQuestion = (userName,userQuestion) =>{    
+let randomNumber = Math.floor(Math.random() * 8)+ 1;
+let eightBall = '';
+     if (randomNumber==1) {        
+        eightBall='It is certain';
+     }else if (randomNumber==2){    
+        eightBall='It is decidedly so';
+    }else if (randomNumber==3){     
+        eightBall='Reply hazy try again';
+    }else if (randomNumber==4){      
+        eightBall='Cannot predict now';
+    }else if (randomNumber==5){     
+        eightBall='Do not count on it';
+    }else if (randomNumber==6){       
+        eightBall='My sources say no';
+    }else if (randomNumber==7){      
+        eightBall='Outlook not so good';
+    }else if (randomNumber==8){       
+            eightBall='Signs point to yes';
+    }else {        
+            eightBall='Invalid item';
+          
+      }
+    
+}
+answerMyQuestion(userName,userQuestion);
+
+
+//4
+let raceNumber = Math.floor(Math.random()*1001);
+let runnerRegistred = false;
+let runnersAge = 18;
+if (runnersAge>=18&&runnerRegistred){
+    raceNumber+=1000;
+}
+raceTime = (runnersAge,runnerRegistred) =>{
+    if (runnersAge>=18&&runnerRegistred)
+{
+    console.log('Early adults run at 9:30 am.' +' Your race number = '+ raceNumber);
+}else if(runnersAge>=18&&!runnerRegistred){
+    console.log('Late adults run at 11:00 am.' + 'Your race number = '+ raceNumber);
+}else if(runnersAge<18){
+    console.log('Youth registrants run at 12:30 pm.' + +'Your race number = '+ raceNumber);
+}else{
+    console.log('You are not our race participant!');
+}
+
+}
+raceTime(18, false);
